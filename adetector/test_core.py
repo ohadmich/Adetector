005b63f023038e60558da6da1c39a792682b0f1e
ohadmich <ohadmich@gmail.com>
2019-06-24 09:53:06 -0400

    added tests for Ad_vs_music_classifier

--- adetector/test_core.py
+++ adetector/test_core.py
@@ -11,6 +11,7 @@ class TestCore(unittest.TestCase):
         self.d = 3 # default clip_duration in seconds
         self.n_mfcc = 13 # default number of mfc coeficients
         self.n_timebins = 130 # default timebins in each clip
+        self.X_sample = np.load('../Data/X_sample.npy') # a sample X array for testing
 
     def test_audio2features_output_shape(self):
         X = core.audio2features(self.audio_path)
@@ -25,9 +26,18 @@ class TestCore(unittest.TestCase):
         std = np.std(X, axis=0)
         zeros_array = np.zeros((self.n_mfcc, self.n_timebins, 1))
         ones_array = np.ones((self.n_mfcc, self.n_timebins, 1))
-        self.assertAlmostEqual(np.allclose(mu, zeros_array), True)
-        self.assertAlmostEqual(np.allclose(std, ones_array), True)
+        self.assertTrue(np.allclose(mu, zeros_array))
+        self.assertTrue(np.allclose(std, ones_array))
 
+    def test_Ad_vs_music_classifier_output_shape(self):
+        prob_over_time = core.Ad_vs_music_classifier(self.X_sample)
+        self.assertAlmostEqual(prob_over_time.shape[0],self.X_sample.shape[0])
+        self.assertAlmostEqual(prob_over_time.shape[1],1)
+
+    def test_Ad_vs_music_classifier_output_value_range(self):
+        prob_over_time = core.Ad_vs_music_classifier(self.X_sample)
+        self.assertTrue(np.max(prob_over_time)<=1)
+        self.assertTrue(np.min(prob_over_time)>=0)
 
 if __name__ == '__main__':
     unittest.main()
