c02aa504db8f7f2ab4695add29ceeae1404b1739
ohadmich <ohadmich@gmail.com>
2019-06-25 14:12:03 -0400

    added a test for moving_average values

--- adetector/test_utils.py
+++ adetector/test_utils.py
@@ -10,6 +10,13 @@ class TestUtils(unittest.TestCase):
         a = np.arange(10)
         avg = utils.moving_average(a, n)
         self.assertAlmostEqual(avg.shape[0],a.shape[0])
+    
+    def test_moving_average_output_value(self):
+        n = 3
+        a = np.arange(10)
+        avg = utils.moving_average(a, n)
+        result = np.array([1.0/3,1,2,3,4,5,6,7,8,26/3])
+        self.assertTrue(np.allclose(avg, result))
 
 if __name__ == '__main__':
     unittest.main()
